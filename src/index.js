0780359e095dd904d77f0c965f41151c3aa62d4c
A.Neumann <andreas.neumann@check24.de>
2018-05-29 16:20:17 +0200

    example2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { Greeter } from "./myModule";
+import { Greeter, StarwarsGreeter } from "./myModule";
 
 const greeter = new Greeter();
 greeter.greet("CHECK24");
+
+const swGreeter = new StarwarsGreeter();
+swGreeter.greet("CHECK24");
--- src/myModule.js
+++ src/myModule.js
@@ -1,7 +1,19 @@
+import * as swNames from "starwars-names";
+
 export class Greeter {
 
     greet(name) {
 
         window.confirm(`Hallo ${name}!`);
+    }
+}
+
+export class StarwarsGreeter {
+
+    greet(name) {
+
+        const character = swNames.random();
+
+        window.confirm(`Hallo ${name}, Grüße von ${character}!`);
     }
 }
